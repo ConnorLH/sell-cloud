7754cf50ee430da80453bd1454dc06370ca66c27
ConnorLH <1206350636@qq.com>
2018-11-04 16:47:44 +0800

    完成自动刷新配置

---
+++ sell-config/src/main/java/com/corner/config/SellConfigApplication.java
@@ -0,0 +1,16 @@
+package com.corner.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class SellConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellConfigApplication.class, args);
+    }
+}
